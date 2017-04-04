5227391a6d8c6227935c965d1feab0fdf5b677a6
JDWOON <jda21@naver.com>
2017-04-05 00:38:56 +0900

    Update Solution.java

--- 001 - 100/035. Search Insert Position (easy)/Solution.java
+++ 001 - 100/035. Search Insert Position (easy)/Solution.java
@@ -12,30 +12,30 @@ public class Solution {
         // return p;
         
         // use binary search library, O(log n) solution.
-		// p = Arrays.binarySearch(nums, target);
-		// if(p<0){
-		// p = -1 - p;
-		// }
+	// p = Arrays.binarySearch(nums, target);
+	// if(p<0){
+	// p = -1 - p;
+	// }
 		
-		// binary search, O(log n) solution
-		int low = 0, high = nums.length-1, mid=0;
-		while(high-low>1){
-			mid = (high + low)/2;
-			if(nums[mid]==target){
-				return mid;
-			}else if(nums[mid]<target){
-				low = mid;
-			}else if(nums[mid]>target){
-				high = mid;
-			}
+	// binary search, O(log n) solution
+	int low = 0, high = nums.length-1, mid=0;
+	while(high-low>1){
+		mid = (high + low)/2;
+		if(nums[mid]==target){
+			return mid;
+		}else if(nums[mid]<target){
+			low = mid;
+		}else if(nums[mid]>target){
+			high = mid;
 		}
-		
-		if(nums[high]<target){
-			return high+1;
-		}else if(nums[low]<target){
-			return high;
-		}else{
-			return low;
-		}
+	}
+
+	if(nums[high]<target){
+		return high+1;
+	}else if(nums[low]<target){
+		return high;
+	}else{
+		return low;
+	}
     }
 }
